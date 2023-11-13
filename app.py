75d7409b0a6c65c9e1498898cc4e42c6c694740a
martindao <35680899+martindao@users.noreply.github.com>
2023-11-13 09:03:00 -0600

    Tune embedding scoring for Assignments

--- app.py
+++ app.py
@@ -96,3 +96,5 @@ if __name__ == "__main__":
 # [2023-04-07] (Assignments) schedule note: Tune embedding scoring for Assignments
 
 # [2023-04-24] (Embeddings) schedule note: Improve document extraction for Embeddings
+
+# [2023-11-13] (Assignments) schedule note: Tune embedding scoring for Assignments
