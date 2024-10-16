922dbf54428f2db5273ec0a69927f8761efd86da
martindao <35680899+martindao@users.noreply.github.com>
2024-10-16 15:51:00 -0600

    Improve document extraction for PRD

--- utils.py
+++ utils.py
@@ -145,3 +145,5 @@ def perform_eda(prd_data, engineers):
 # [2024-09-03] (Extraction) schedule note: Improve document extraction for Extraction
 
 # [2024-10-01] (Embeddings) schedule note: Improve document extraction for Embeddings
+
+# [2024-10-16] (PRD) schedule note: Improve document extraction for PRD
