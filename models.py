ae3126a95ae877c67a99a49f429832ec5cd88906
martindao <35680899+martindao@users.noreply.github.com>
2024-11-05 16:59:00 -0600

    Document PRD automation results for Assignments

--- models.py
+++ models.py
@@ -98,3 +98,5 @@ class ReinforcementLearningModel(AdvancedNLPModel):
 # [2024-10-04] (Extraction) schedule note: Document PRD automation results for Extraction
 
 # [2024-10-22] (PRD) schedule note: Add diagnostics for PRD
+
+# [2024-11-05] (Assignments) schedule note: Document PRD automation results for Assignments
